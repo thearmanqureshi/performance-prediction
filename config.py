a036238afe50daa5cc5bc4c60e447b8e60e6bcf5
Arman Qureshi <thearmanqureshi@gmail.com>
2024-11-17 17:13:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,21 @@
+# config.py
 import os
 from datetime import timedelta
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
+    SECRET_KEY = os.getenv('SECRET_KEY')
     MONGO_URI = os.getenv('MONGO_URI')
     SESSION_COOKIE_SECURE = True
     PERMANENT_SESSION_LIFETIME = timedelta(minutes=30)
+    MODEL_URL = os.getenv('MODEL_URL')
+    SCALER_URL = os.getenv('SCALER_URL')
+    MAX_PREDICTION_SCORE = 98
+    MAX_AGE = 100
+    MAX_STUDY_HOURS = 24
+    MAX_FAILURES = 10
 
 class ProductionConfig(Config):
     DEBUG = False
