7a052a7e78a1b53f8b36697132816a6cefc81172
Arman Qureshi <thearmanqureshi@gmail.com>
2024-11-17 17:55:57 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -2,4 +2,6 @@ from app import app
 
 # Run the application
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Use the PORT environment variable provided by Render
+    port = int(os.environ.get('PORT', 5000))  # Default to 5000 for local testing
+    app.run(host='0.0.0.0', port=port, debug=app.config['DEBUG'])
