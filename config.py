4985ac37814834156e9ea9246061707fdfff5996
Arman Qureshi <thearmanqureshi@gmail.com>
2024-11-22 22:29:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-# config.py
 import os
 from datetime import timedelta
 from dotenv import load_dotenv
@@ -10,20 +9,24 @@ class Config:
     MONGO_URI = os.getenv('MONGO_URI')
     SESSION_COOKIE_SECURE = True
     PERMANENT_SESSION_LIFETIME = timedelta(minutes=30)
-    MODEL_URL = os.getenv('MODEL_URL')
-    SCALER_URL = os.getenv('SCALER_URL')
+    MODEL_URL = os.getenv('MODEL_URL')  # URL for the hosted .tflite model
+    SCALER_URL = os.getenv('SCALER_URL')  # URL for the hosted scaler .pkl file
     MAX_PREDICTION_SCORE = 98
     MAX_AGE = 100
     MAX_STUDY_HOURS = 24
     MAX_FAILURES = 10
+    # Optional: Add other environment-specific configurations as needed.
 
 class ProductionConfig(Config):
     DEBUG = False
     TESTING = False
     SESSION_COOKIE_HTTPONLY = True
     SESSION_COOKIE_SAMESITE = 'Lax'
+    # Any additional production-specific settings can be added here.
 
 class DevelopmentConfig(Config):
     DEBUG = True
     TESTING = True
     SESSION_COOKIE_SECURE = False
+    # You can also add specific development configurations if needed.
+
